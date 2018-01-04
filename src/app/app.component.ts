ef734b2630879f830ab21aa3c9fa24336dce7cad
PLAYTIKA\pavelkok <pavelkok@playtika.com>
2018-01-04 18:25:09 +0300

    two commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('querycli') querycli: ElementRef;
+
+  onClick() {
+    const queryCliValue = this.querycli.nativeElement.value;
+
+    if (!queryCliValue) {
+      return false;
+    }
+
+    const queryNodes = queryCliValue.split('\n');
+
+    const nodesTree = queryNodes.reduce((acc, node) => {
+      const parseNode = node.trim().split('->');
+      const parent = parseNode[0].trim();
+      const children = parseNode[1].length > 1 ? parseNode[1].split(',').map(child => child.trim()) : parseNode[1];
+
+      const addChildren = () => {
+        return children.reduce((accChield, child) => {
+          accChield[child] = null;
+          return accChield;
+        }, {});
+      };
+
+      const searchNesting = (obj) => {
+        Object.keys(obj).forEach(function(key) {
+          if (obj[key][parent] === null) {
+            obj[key][parent] = addChildren();
+          } else {
+            searchNesting(obj)
+          }
+        });
+      }; 
+
+      if (Object.keys(acc).length === 0) {
+        acc[parent] = addChildren();
+      } else {
+        searchNesting(acc)
+
+        return acc;
+      } 
+
+      return acc;
+    }, {});
+
+    console.log(nodesTree);
+  }
 }
