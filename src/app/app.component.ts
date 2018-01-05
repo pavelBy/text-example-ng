5f423d5db94131bfff387a68cc045228279c8df6
PLAYTIKA\pavelkok <pavelkok@playtika.com>
2018-01-05 16:00:28 +0300

    fix create nodes tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+import gojs from 'gojs';
 
 @Component({
   selector: 'app-root',
@@ -16,36 +17,32 @@ export class AppComponent {
     }
 
     const queryNodes = queryCliValue.split('\n');
+    const mapChildAndParent = {};
 
     const nodesTree = queryNodes.reduce((acc, node) => {
       const parseNode = node.trim().split('->');
       const parent = parseNode[0].trim();
       const children = parseNode[1].length > 1 ? parseNode[1].split(',').map(child => child.trim()) : parseNode[1];
 
-      const addChildren = () => {
+      const addDataToTree = () => {
         return children.reduce((accChield, child) => {
           accChield[child] = null;
+          mapChildAndParent[child] = parent;
           return accChield;
         }, {});
       };
 
-      const searchNesting = (obj) => {
-        Object.keys(obj).forEach(function(key) {
-          if (obj[key][parent] === null) {
-            obj[key][parent] = addChildren();
-          } else {
-            searchNesting(obj)
-          }
-        });
-      }; 
-
-      if (Object.keys(acc).length === 0) {
-        acc[parent] = addChildren();
-      } else {
-        searchNesting(acc)
-
-        return acc;
-      } 
+      if (mapChildAndParent[parent]) {
+        const firstLevelParent = mapChildAndParent[parent];
+        const twoLevelParent = mapChildAndParent[firstLevelParent];
+        if (twoLevelParent) {
+          acc[twoLevelParent][firstLevelParent][parent] = addDataToTree();
+        } else {
+          acc[firstLevelParent][parent] = addDataToTree();
+        }
+      } else if (Object.keys(acc).length === 0 || !mapChildAndParent[parent]) {
+        acc[parent] = addDataToTree();
+      }
 
       return acc;
     }, {});
